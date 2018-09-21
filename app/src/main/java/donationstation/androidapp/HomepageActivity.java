2f77c284b7cf8b62baed8bae4782fdada190b818
kaushic <kaushic98@bitbucket.org/>
2018-09-21 14:47:21 -0400

    added login functionality

---
+++ app/src/main/java/donationstation/androidapp/HomepageActivity.java
@@ -0,0 +1,25 @@
+package donationstation.androidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    public void logout(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
