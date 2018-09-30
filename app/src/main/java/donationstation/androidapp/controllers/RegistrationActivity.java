79db7a78caca59575654f9ac94f531744d9cd255
kaushic <kaushic98@bitbucket.org/>
2018-09-30 00:25:05 -0400

    updated dropdown to show account options

--- app/src/main/java/donationstation/androidapp/controllers/RegistrationActivity.java
+++ app/src/main/java/donationstation/androidapp/controllers/RegistrationActivity.java
@@ -4,19 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
 
 import donationstation.androidapp.R;
+import donationstation.androidapp.model.Registration;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private Spinner accountSpinner;
+
+    private Registration _account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //grab dialog widgets for info later on
+        accountSpinner = findViewById(R.id.accountType);
+
+        //array of possible account types
+        String [] accountTypes = {"Select Account Type","Admin", "User", "Manager", "Employee"};
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, accountTypes);
+        accountSpinner.setAdapter(adapter);
     }
 
     public void main(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        _account.setAccount(parent.getItemAtPosition(position).toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        _account.setAccount("Guest");
     }
 }
--- app/src/main/java/donationstation/androidapp/model/Registration.java
+++ app/src/main/java/donationstation/androidapp/model/Registration.java
@@ -1,5 +1,25 @@
 package donationstation.androidapp.model;
 
 public class Registration {
+    private String name;
+    private String email;
+    private String password;
+    private boolean accountState;
+    private String accountType;
 
+    public Registration() {
+        name = "n/a";
+        email = "n/a";
+        password = "n/a";
+        accountState = true;
+        accountType = "n/a";
+    }
+
+    public void setAccount(String acc) {
+        accountType = acc;
+    }
+
+    public String getAccount() {
+        return accountType;
+    }
 }
