30754a9d97cfa52791f9eeb158be2b34dbdc828b
Joonyeol Oh <zbqmal@gmail.com>
2018-10-11 16:39:17 -0400

    Test recyclerView

--- app/src/main/java/donationstation/androidapp/controllers/LocationListActivity.java
+++ app/src/main/java/donationstation/androidapp/controllers/LocationListActivity.java
@@ -1,18 +1,15 @@
 package donationstation.androidapp.controllers;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.List;
 
 import donationstation.androidapp.R;
 
@@ -22,5 +19,49 @@ public class LocationListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_list);
+
+        RecyclerView locationLists = (RecyclerView) findViewById(R.id.locationLists);
+        locationLists.setLayoutManager(new LinearLayoutManager(this));
+        String[] languages = {"Java", "JavaScript", "C#", "php", "C", "C++", "Python", "Java", "JavaScript", "C#", "php", "C", "C++", "Python"};
+        locationLists.setAdapter(new locationListAdapter(languages));
+    }
+
+
+    // RecyclerView Adapter
+    public class locationListAdapter extends RecyclerView.Adapter<locationListAdapter.locationListViewHolder> {
+
+        private String[] data;
+        public locationListAdapter(String[] data) {
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public locationListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.list_location_layout, parent, false);
+            return new locationListViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull locationListViewHolder holder, int position) {
+            String title = data[position];
+            holder.txtTitle.setText(title);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.length;
+        }
+
+        public class locationListViewHolder extends RecyclerView.ViewHolder {
+            ImageView imgIcon;
+            TextView txtTitle;
+            public locationListViewHolder(View itemView) {
+                super(itemView);
+                imgIcon = (ImageView) itemView.findViewById(R.id.imgIcon);
+                txtTitle = (TextView) itemView.findViewById(R.id.txtTitle);
+            }
+        }
     }
 }
