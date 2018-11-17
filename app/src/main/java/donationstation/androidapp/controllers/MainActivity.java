02ea62aa047c569245d29ae009618dde7fb348e5
joon <zbqmal@gmail.com>
2018-11-17 14:49:46 -0500

    Decorating with images

--- app/src/main/java/donationstation/androidapp/controllers/MainActivity.java
+++ app/src/main/java/donationstation/androidapp/controllers/MainActivity.java
@@ -1,9 +1,13 @@
 package donationstation.androidapp.controllers;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //mAuth = FirebaseAuth.getInstance();
         // test
     }
