eee4789f0fc261da90f9eda210ab6720a5f4f25f
Ricky Rosa <ricardorcidad@gmail.com>
2018-12-01 15:55:18 -0500

    Google sign in

--- app/src/main/java/donationstation/androidapp/controllers/LoginActivity.java
+++ app/src/main/java/donationstation/androidapp/controllers/LoginActivity.java
@@ -15,6 +15,10 @@ import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,13 +31,14 @@ import donationstation.androidapp.R;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private GoogleSignInClient mGoogleSignInClient;
     private final DatabaseReference ref = FirebaseDatabase.
             getInstance().getReference().child("users");
 
@@ -59,6 +64,33 @@ public class LoginActivity extends AppCompatActivity {
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+
+    private void signIn() {
+//        System.out.println("Bouncy");
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 0);
+        Intent intent = new Intent(this, UserHomepageActivity.class);
+        intent.putExtra("userType", "User");
+        startActivity(intent);
     }
 
 
@@ -223,6 +255,11 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, UserHomepageActivity.class);
         intent.putExtra("userType", "User");
         startActivity(intent);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
 
--- app/src/main/java/donationstation/androidapp/controllers/UserHomepageActivity.java
+++ app/src/main/java/donationstation/androidapp/controllers/UserHomepageActivity.java
@@ -3,9 +3,19 @@ package donationstation.androidapp.controllers;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
+
 
 import donationstation.androidapp.R;
 
@@ -15,6 +25,8 @@ import donationstation.androidapp.R;
 public class UserHomepageActivity extends Activity {
 
     private String userType;
+//    private GoogleSignInClient mGoogleSignInClient;
+//    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,18 @@ public class UserHomepageActivity extends Activity {
         } else {
             userType = bundle.getString("userType");
         }
+
+//        findViewById(R.id.g_sign_out).setOnClickListener((View.OnClickListener) this);
+//
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
+//        mGoogleApiClient.connect();
+//        super.onStart();
+
     }
     /**
      *
@@ -38,7 +62,41 @@ public class UserHomepageActivity extends Activity {
         mAuth.signOut();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+//        signOut();
+//        System.out.println("Bouncy");
+//        System.out.println(mGoogleApiClient.isConnected());
+//        if (mGoogleApiClient.isConnected()) {
+//                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+//            mGoogleApiClient.disconnect();
+//            mGoogleApiClient.connect();
+//        }
     }
+
+
+
+//    public void onClick(View v) {
+////        System.out.println("Bouncy");
+//        switch (v.getId()) {
+//            case R.id.g_sign_out:
+//                if (mGoogleApiClient.isConnected()) {
+////                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+//                    mGoogleApiClient.disconnect();
+//                    mGoogleApiClient.connect();
+//                }
+//                break;
+//        }
+//    }
+
+//    private void signOut() {
+//        System.out.println("Bouncy");
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//
+//                    }
+//                });
+//    }
     /**
      *
      * @param view view to pass to LocationListActivity
